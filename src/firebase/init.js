a8ae8be09da2252711eb865a795e4d540b3c14fa
Miguel Torres <migueltg93@gmail.com>
2018-07-04 20:25:33 -0500

    Initial commit 🚀

---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBicpPhxo0sQ9fODBoNa5-kT7zFFfG1D4Y",
+  authDomain: "geo-wizards.firebaseapp.com",
+  databaseURL: "https://geo-wizards.firebaseio.com",
+  projectId: "geo-wizards",
+  storageBucket: "geo-wizards.appspot.com",
+  messagingSenderId: "999127908302"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+export default firebaseApp.firestore()
