748084bccee320d8d9c4fcd140b9173c79376898
Miguel Torres <migueltg93@gmail.com>
2018-07-05 11:38:27 -0500

    Added GMaps

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="#" class="brand-logo left">GeoWizards!</a>
+        <ul class="right">
+          <li> <a href="#">Sign up</a> </li>
+          <li> <a href="#">Log in</a> </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
